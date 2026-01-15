96a0485b33d4ee7265b75482b9c2ccfae51e9c3d
cousinfabien <cousin.fabien3@gmail.com>
2026-01-15 13:55:02 +0100

    Add Advanced Task 1 : Smile in the mirror

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print(''.join(f'{chr(122-i) if i % 2 == 0 else chr(90 - ((i//2)*2 + 1))}' for i in range(26)), end='')
-
+print(''.join('{0}'.format(chr(122-i) if i % 2 == 0 else chr(90 - ((i//2)*2 + 1))) for i in range(26)), end='')
