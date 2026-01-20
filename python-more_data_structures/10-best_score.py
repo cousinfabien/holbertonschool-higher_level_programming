bdfcae1bbb31593f4a93b43435dbcfb1b384c081
cousinfabien <cousin.fabien3@gmail.com>
2026-01-20 13:56:47 +0100

    Add Task 10 : Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = None
+    best_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_value = value
+            best_key = key
+    return best_key
 
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
