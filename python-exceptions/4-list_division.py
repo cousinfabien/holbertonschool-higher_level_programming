05b47f712a546e1188c3f25b233fbae160096436
cousinfabien <cousin.fabien3@gmail.com>
2026-01-21 12:28:18 +0000

    Add Task 4: Divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            if not isinstance(a, (int, float)) \
+               or not isinstance(b, (int, float)):
+                raise TypeError
+            div = a / b
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            result_list.append(div)
+    return result_list
