cba1cfce43f9ffe451089a1bf452595889333656
cousinfabien <cousin.fabien3@gmail.com>
2026-01-21 08:47:40 +0000

    Fix Pycodestyle

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def new_in_list (my_list, id, element):
+def new_in_list(my_list, id, element):
     listcopy = my_list.copy()
     if id < 0 or id >= len(my_list):
         return listcopy
