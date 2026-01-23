f893d49dc5ca736795be68b68c76b361e810b72b
cousinfabien <cousin.fabien3@gmail.com>
2026-01-23 13:28:28 +0000

    Add Task 2 : Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that prints a person's full name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first_name> <last_name>".
+
+    Args:
+        first_name (str): first name of the person
+        last_name (str, optional): last name of the person. Defaults to "".
+
+    Raises:
+        TypeError: if first_name or last_name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
