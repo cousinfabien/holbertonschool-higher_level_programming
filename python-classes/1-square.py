e233c8f118d34c9e514dfd25332a488a1bf00a55
cousinfabien <cousin.fabien3@gmail.com>
2026-01-26 10:42:48 +0100

    Add Task 1 : Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""This module defines a class Square with a private size attribute."""
 
+
+class Square:
+    """Represent a square with a private size attribute."""
+    def __init__(self, size):
+        """Initialize a new Square instance.
+        Args:
+            size (any type): Size of square.
+        """
+        self.__size = size
