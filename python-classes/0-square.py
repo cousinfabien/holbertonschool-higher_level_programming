8c102c6ebcfdad286336011d8ac9416f366b0c61
cousinfabien <cousin.fabien3@gmail.com>
2026-01-26 10:41:38 +0100

    Add Task 0 : My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This module defines an empty class Square."""
 
+
 class Square:
     """Represent a square."""
     pass
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
