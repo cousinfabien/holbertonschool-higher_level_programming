000e02bfa4643ce6a6f3f5335e6339d563275011
cousinfabien <cousin.fabien3@gmail.com>
2026-02-02 14:10:37 +0100

    Add Task 3 : Same class or inherit from

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a function to check if
+an object is an instance of or inherits from a specified class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of,
+     or inherits from, the specified class; otherwise False."""
+    return isinstance(obj, a_class)
