4d4ac61dad901f017d8feb0fc53655dbc35d8102
cousinfabien <cousin.fabien3@gmail.com>
2026-02-02 10:50:09 +0100

    Add task 0 : Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""This modiule defines a function that returns
+the attributes and methods of an object."""
+
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)
