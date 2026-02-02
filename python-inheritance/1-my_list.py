167efc8965d799abfe92cc1ea723e20d501373f1
cousinfabien <cousin.fabien3@gmail.com>
2026-02-02 11:32:09 +0100

    Add Task 1 : My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a class that extends the built-in list class."""
+
+
+class MyList(list):
+    """Represents a list that can print itself sorted."""
+
+    def print_sorted(self):
+        """Prints the list sorted in ascending order."""
+        print(sorted(self))
