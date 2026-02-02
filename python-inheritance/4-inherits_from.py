08733c734998593d38d69fdbf01a0fc17afcd008
cousinfabien <cousin.fabien3@gmail.com>
2026-02-02 14:30:18 +0100

    Add Task 4 : Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a function to check if
+an object is an instance of a class that inherits from a specified class."""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of
+    a class that inherited (directly or indirectly)
+    from the specified class; otherwise False."""
+    return isinstance(obj, a_class) and type(obj) is not a_class
