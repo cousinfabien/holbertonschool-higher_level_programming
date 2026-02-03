625e2056c563730c6a5d4f30ca264a897bdb8570
cousinfabien <cousin.fabien3@gmail.com>
2026-02-03 09:49:13 +0100

    Add Task 9 : Full Rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""This module defines a Rectangle class with
+area calculation and string representation."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle defined by width and height."""
+
+    def __init__(self, width, height):
+        """Initializes a rectangle with validated width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
