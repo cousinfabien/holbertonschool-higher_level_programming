ea420c916c1070d3e1a2b45ce26c26d9269e3aa2
cousinfabien <cousin.fabien3@gmail.com>
2026-02-03 14:47:20 +0100

    Add Task 0 : Abstract Animal Class and its Subclasses

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Abstract Animal class with Dog and Cat subclasses."""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract base class representing an animal."""
+
+    @abstractmethod
+    def sound(self):
+        """Abstract method to be implemented by subclasses."""
+        pass
+
+
+class Dog(Animal):
+    """Dog class, subclass of Animal"""
+
+    def sound(self):
+        """Returns the sound of a dog"""
+        return "Bark"
+
+
+class Cat(Animal):
+    """Cat class, subclass of Animal"""
+
+    def sound(self):
+        """Returns the sound of a cat"""
+        return "Meow"
