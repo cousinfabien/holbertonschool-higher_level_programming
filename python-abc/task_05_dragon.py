5dcf2143a56ffa85e4cb2555316e48ae8d5012d0
cousinfabien <cousin.fabien3@gmail.com>
2026-02-04 09:54:00 +0000

    Add Task 5 : The Mystical Dragon - Mastering Mixins

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Demonstrates the use of mixins to add behavior to a class."""
+
+class SwimMixin:
+    """Mixin class that adds swimming capability."""
+
+    def swim(self):
+        """Print a swimming message."""
+        print("The creature swims!")
+
+class FlyMixin:
+    """Mixin class that adds flying capability."""
+
+    def fly(self):
+        """Print a flying message."""
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    """A dragon that can both swim and fly."""
+
+    def roar(self):
+        """Print a roaring message."""
+        print("The dragon roars!")
