fbf28b26f6eee0b11b492be3bc11364570789044
cousinfabien <cousin.fabien3@gmail.com>
2026-02-04 09:09:09 +0000

    Fix Task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list sorted in ascending order."""
-        new_list = self.copy()
-        new_list.sort()
-        print(new_list)
+        print([x for x in sorted(self)])
