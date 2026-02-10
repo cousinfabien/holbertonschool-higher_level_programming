da01ac5d84c845aaf67eb6e92c528b9ca5917818
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 10:03:33 +0000

    add task 5

--- python-input-output/5-save_to_json_file.py
+++ python-input-output/5-save_to_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""This module provides a function to save
+a Python object to a file in JSON format."""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes a Python object to a text file using its JSON representation."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
