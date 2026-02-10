67c0f8a274536b7861cb4ce7729a401bf95d42ca
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 09:53:52 +0000

    Add Task 4

--- python-input-output/4-from_json_string.py
+++ python-input-output/4-from_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This module provides a function to convert
+a JSON string to a Python object."""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns the Python object represented by a JSON string."""
+    return json.loads(my_str)
