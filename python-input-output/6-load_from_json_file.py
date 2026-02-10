fbf5a77e8da50cefa3b0edf4460f9b89fac2a413
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 12:08:28 +0000

    Add task 6

--- python-input-output/6-load_from_json_file.py
+++ python-input-output/6-load_from_json_file.py
@@ -6,7 +6,7 @@ a Python object to a file in JSON format."""
 import json
 
 
-def load_to_json_file(filename):
+def load_from_json_file(filename):
     """Creates a Python object to a text file using its JSON representation."""
     with open(filename, "r", encoding="utf-8") as f:
         json.load(f)
