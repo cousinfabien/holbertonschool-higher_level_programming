5874c505bd0686fe2e293a41aec6423ba6795a42
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 09:06:04 +0000

    Add Task 0 : Read File

--- python-input-output/0-read_file.py
+++ python-input-output/0-read_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""This module provides
+a function to read and print a text file."""
+
+
 def read_file(filename=""):
+    """Reads a text file (UTF8)
+    and prints its content to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
