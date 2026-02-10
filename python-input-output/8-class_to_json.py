538193a2c49a6e6acf86255eda8b7f923952b795
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 12:16:24 +0000

    Add task 8

--- python-input-output/8-class_to_json.py
+++ python-input-output/8-class_to_json.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This module provides a function to convert a class
+object to a JSON-serializable dictionary."""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description of a class instance with simple
+    data structures (list, dict, str, int, bool) for JSON serialization.
+    """
+    return obj.__dict__.copy()
