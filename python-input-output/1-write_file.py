eb438b08f0278300a696d38647a5decd6fa80178
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 09:19:30 +0000

    Add Task 1

--- python-input-output/1-write_file.py
+++ python-input-output/1-write_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""This module provides a function to
+write a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file and
+    returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
