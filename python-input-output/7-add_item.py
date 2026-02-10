ef65cbdb8c2615fb384ca3f47c86b20505f13a4b
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 12:13:24 +0000

    Add Task 7

--- python-input-output/7-add_item.py
+++ python-input-output/7-add_item.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Adds all arguments to a
+Python list and saves them to a JSON file."""
+
+import sys
+save_module = __import__("5-save_to_json_file")
+load_module = __import__("6-load_from_json_file")
+
+save_to_json_file = save_module.save_to_json_file
+load_from_json_file = load_module.load_from_json_file
+
+filename = "add_item.json"
+
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
