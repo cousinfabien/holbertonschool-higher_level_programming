91037286fe8a1457c2b7a586e10cc1be067e456b
cousinfabien <cousin.fabien3@gmail.com>
2026-02-10 09:45:40 +0000

    Add Task 3

--- python-input-output/3-json_to_string.py
+++ python-input-output/3-json_to_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""This module provides a function
+to convert a Python object to JSON string."""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of a Python object as a string."""
+    return json.dumps(my_obj)
