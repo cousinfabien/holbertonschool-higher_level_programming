0a24d0d20dbbd9c2c5f41cac86885638333de439
cousinfabien <cousin.fabien3@gmail.com>
2026-02-11 12:52:20 +0000

    Add Task 0

--- python-serialisation/task_00_basic_serialization.py
+++ python-serialisation/task_00_basic_serialization.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Basic serialization module."""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a dictionary and save it to a JSON file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """Load JSON data from a file and
+    return the corresponding dictionary."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
