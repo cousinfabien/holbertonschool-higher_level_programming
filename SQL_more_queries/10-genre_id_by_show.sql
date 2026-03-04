f99f97ec4791db015d98586f767eb6e51606ceab
cousinfabien <cousin.fabien3@gmail.com>
2026-03-04 12:34:27 +0000

    Add Task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all tv shows with at least one genre: title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
