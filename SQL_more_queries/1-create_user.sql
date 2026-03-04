b4eeea506314393220bb022b3b34e2af2205e77c
cousinfabien <cousin.fabien3@gmail.com>
2026-03-04 12:09:21 +0000

    Add task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates MySQL user user_0d_1 with all privileges and a password, without failing if user exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
